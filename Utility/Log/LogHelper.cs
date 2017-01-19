5577084a1cd3170df6999b119a5c08a4f08da9c0
LiWeiJia <liweijia159@sina.com>
2017-01-19 10:50:49 +0800

    logger

---
+++ Utility/Log/LogHelper.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility.Log
+{
+    public class LogHelper
+    {
+        private ILog logger;
+
+        public LogHelper(Type type)
+        {
+            logger = LogManager.GetLogger(type);
+        }
+    }
+}
